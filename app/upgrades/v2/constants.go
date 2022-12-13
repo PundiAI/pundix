ff2091fc5c49349704a1f163dd65752354684d90
fx0x55 <dennis@wokoworks.com>
2022-12-13 14:48:59 +0800

    fix: remove unused code

--- app/upgrades/v2/constants.go
+++ app/upgrades/v2/constants.go
@@ -13,8 +13,6 @@ import (
 const (
 	// UpgradeName is the shared upgrade plan name for mainnet
 	UpgradeName = "pxv2"
-	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/pundix/pundix/releases/download/v0.2.0/pundix_0.2.0_Darwin_arm64.tar.gz","darwin/amd64":"https://github.com/pundix/pundix/releases/download/v0.2.0/pundix_0.2.0_Darwin_amd64.tar.gz","linux/arm64":"https://github.com/pundix/pundix/releases/download/v0.2.0/pundix_0.2.0_Linux_arm64.tar.gz","linux/amd64":"https://github.com/pundix/pundix/releases/download/v0.2.0/pundix_0.2.0_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/pundix/pundix/releases/download/v0.2.0/pundix_0.2.0_Windows_x86_64.zip"}}'`
 )
 
 const (
